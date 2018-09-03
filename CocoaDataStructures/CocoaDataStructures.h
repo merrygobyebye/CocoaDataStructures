e0f77c43dfb807ab575359442c6aef2f40b642e2
merrygobyebye <eric.94253@gmail.com>
2018-09-03 13:21:13 -0700

    Initial Commit

---
+++ CocoaDataStructures/CocoaDataStructures.h
@@ -0,0 +1,19 @@
+//
+//  CocoaDataStructures.h
+//  CocoaDataStructures
+//
+//  Created by Eric Fisher on 9/3/18.
+//  Copyright © 2018 merrygobyebye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CocoaDataStructures.
+FOUNDATION_EXPORT double CocoaDataStructuresVersionNumber;
+
+//! Project version string for CocoaDataStructures.
+FOUNDATION_EXPORT const unsigned char CocoaDataStructuresVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaDataStructures/PublicHeader.h>
+
+
