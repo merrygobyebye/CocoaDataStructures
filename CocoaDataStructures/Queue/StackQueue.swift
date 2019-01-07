1ef30fed116aafa6e9cc5c76734a076b9c8a0a9d
merrygobyebye <eric.94253@gmail.com>
2019-01-06 19:23:31 -0800

    * Add StackQueue and unit test

---
+++ CocoaDataStructures/Queue/StackQueue.swift
@@ -0,0 +1,49 @@
+//
+//  StackQueue.swift
+//  CocoaDataStructures
+//
+//  Created by Eric Fisher on 1/6/19.
+//  Copyright © 2019 merrygobyebye. All rights reserved.
+//
+
+import Foundation
+
+class StackQueue<T> where T: Comparable {
+    
+    private var dequeueStack: ArrayStack<T> = ArrayStack<T>()
+    private var enqueueStack: ArrayStack<T> = ArrayStack<T>()
+    
+    private(set) var count: UInt = 0
+    
+    func enqueue(value: T) {
+        enqueueStack.push(value)
+        
+        count += 1
+    }
+    
+    func dequeue() -> T? {
+        if count == 0 {
+            return nil
+        }
+        
+        count -= 1
+        
+        if dequeueStack.count() == 0 {
+            while enqueueStack.count() > 0 {
+                dequeueStack.push(enqueueStack.pop()!)
+            }
+        }
+        
+        return dequeueStack.pop()
+    }
+    
+    func front() -> T? {
+        if dequeueStack.count() == 0 {
+            while enqueueStack.count() > 0 {
+                dequeueStack.push(enqueueStack.pop()!)
+            }
+        }
+        
+        return dequeueStack.peek()
+    }
+}
---
+++ CocoaDataStructuresTests/Queue/StackQueueTest.swift
@@ -0,0 +1,63 @@
+//
+//  StackQueueTest.swift
+//  CocoaDataStructuresTests
+//
+//  Created by Eric Fisher on 1/6/19.
+//  Copyright © 2019 merrygobyebye. All rights reserved.
+//
+
+import XCTest
+@testable import CocoaDataStructures
+
+class StackQueueTest: XCTestCase {
+
+    func testStackQueue() {
+        let queue = StackQueue<String>()
+        
+        var first = "first"
+        var second = "second"
+        var third = "third"
+        
+        XCTAssertEqual(queue.count, 0)
+        XCTAssertNil(queue.front())
+        
+        queue.enqueue(value: first)
+        XCTAssertEqual(queue.count, 1)
+        XCTAssertEqual(queue.front(), first)
+        queue.enqueue(value: second)
+        XCTAssertEqual(queue.count, 2)
+        XCTAssertEqual(queue.front(), first)
+        queue.enqueue(value: third)
+        XCTAssertEqual(queue.count, 3)
+        XCTAssertEqual(queue.front(), first)
+        
+        XCTAssertEqual(queue.dequeue(), first)
+        XCTAssertEqual(queue.dequeue(), second)
+        XCTAssertEqual(queue.dequeue(), third)
+        
+        XCTAssertEqual(queue.count, 0)
+        XCTAssertNil(queue.front())
+        
+        first = "fourth"
+        second = "fifth"
+        third = "sixth"
+        
+        queue.enqueue(value: first)
+        XCTAssertEqual(queue.count, 1)
+        XCTAssertEqual(queue.front(), first)
+        queue.enqueue(value: second)
+        XCTAssertEqual(queue.count, 2)
+        XCTAssertEqual(queue.front(), first)
+        queue.enqueue(value: third)
+        XCTAssertEqual(queue.count, 3)
+        XCTAssertEqual(queue.front(), first)
+        
+        XCTAssertEqual(queue.dequeue(), first)
+        XCTAssertEqual(queue.dequeue(), second)
+        XCTAssertEqual(queue.dequeue(), third)
+        
+        XCTAssertEqual(queue.count, 0)
+        XCTAssertNil(queue.front())
+    }
+
+}
